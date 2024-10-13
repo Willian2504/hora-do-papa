290c22a69035264020531d04dc0bf3f01a10f16e
Willian2504 <williancrisostomo16@gmail.com>
2024-10-13 13:55:07 -0300

    update no cep
    tentei usar api de cep mas sem sucesso ainda

--- src/javascript/cep.js
+++ src/javascript/cep.js
@@ -9,8 +9,20 @@ function consultaEndereco() {
     let url = `https://viacep.com.br/ws/${cep}/json/`;
 
     fetch(url).then(function(response){
-        
+        response.json().then(function(data){
+            console.log (data)
+            mostrarEndereco(data);
+        })
     });
 
 
+}
+
+function mostrarEndereco(dados) {
+    let resultado = document.querySelector('#resultado');
+
+    resultado.innerHTML = `<p>Endereço: ${dados.logradouro}</p>
+                           <p>Complemento: ${dados.complemento}</p>
+                           <p>Bairro: ${dados.bairro}</p>
+                           <p>Cidade: ${dados.localidade} - ${dados.uf}</p>`
 }
